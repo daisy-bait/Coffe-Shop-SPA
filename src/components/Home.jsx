462b36b41ce0e68c9d36f9991b194c2842101787
daisy-bait <u20231213624@usco.edu.co>
2025-05-16 17:05:14 -0500

    feat(home): adjust title for better visual impact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,21 +3,29 @@ import mainCoffeBackground from "../assets/img/coffe-soft-header.svg"
 const Home = () => {
     return (
         <div className="uk-position-top">
-            <div className="uk-background-cover uk-height-large uk-light first-child-adjustment uk-flex"
+            <div className="uk-background-cover uk-height-viewport uk-light first-child-adjustment uk-flex uk-position-relative"
                 style={{
                     backgroundImage: `url(${mainCoffeBackground})`,
                 }}
             >
-                <div className="uk-container uk-margin-large-top">
-                    <h1> Título Tienda Café </h1>
-                    <div className="uk-overlay uk-overlay-primary uk-padding-small" >
-                        <p>Bienvenido a <strong>Nombre del Café</strong>, un lugar donde cada taza de café
-                            cuenta una historia. Aquí encontrarás la combinación perfecta de calidad,
-                            comodidad y calidez para hacer de tus mañanas algo especial.
-                        </p>
+                <div className="uk-container uk-margin-large-top uk-height-1-1">
+                    <div className="uk-position-center-right uk-position-medium"
+                    style={{
+                            maxWidth: '50%',
+                            padding: '20px'
+                        }}>
+                        <h1 className="uk-heading-large">Coffe Shop SPA</h1>
+                        <div className="uk-overlay uk-overlay-primary uk-padding-small" >
+                            <p>Bienvenido a <strong>Nombre del Café</strong>, un lugar donde cada taza de café
+                                cuenta una historia. Aquí encontrarás la combinación perfecta de calidad,
+                                comodidad y calidez para hacer de tus mañanas algo especial.
+                            </p>
+                        </div>
+
                     </div>
                 </div>
             </div>
+            {/*
             <div className="uk-background-muted uk-padding-large" >
                 <h4> Menú </h4>
                 <div className="uk-overlay uk-overlay-primary uk-padding-small" >
@@ -27,6 +35,7 @@ const Home = () => {
                     </p>
                 </div>
             </div>
+            */}
         </div>
     )
 }
